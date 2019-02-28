025c6f2b8d2a752262dd4faa82e172c2c9ca7955
DESKTOP-0D452KE\izahi <izahir.cascante@outlook.com>
2019-02-27 20:17:45 -0600

    1.First commit MasLarga DONE 2.0

--- src/cenfotec/kataanalytics/ac/cr/Analytics.java
+++ src/cenfotec/kataanalytics/ac/cr/Analytics.java
@@ -1,52 +1,53 @@
 package cenfotec.kataanalytics.ac.cr;
 
-import java.util.Scanner;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Analytics {
 
-		public static void main(String[] args) {
-			
-//			@SuppressWarnings("resource")
-//			Scanner sc = new Scanner(System.in);
-//			String texto;
-//			
-//			System.out.println("Ingrese el texto: ");
-//			texto = sc.nextLine(); 
-//			
-//			masLarga(texto);
+	public static void main (String [] args) {
+		String txt = "casa casita casototota";
+		
+		masLarga(txt);
+		
+		
+	}
+
+	
+	public static String masLarga(String cadenaPalabra) {
+		
+		String palabra = cadenaPalabra; 
+		
+		StringTokenizer cadena = new StringTokenizer (palabra);
+		
+		String sCadena;	    
+	    String palabraMasGrande = "";
+	 
+	    int palabraMasGrandeSize = 0;
+	 
+	    
+	    
+	    int palabras = cadena.countTokens();
+	    
+	    for (int x=0; x<palabras; x++){
+	    	
+	    	sCadena = cadena.nextToken();
+	    	
+	    	if (sCadena.length() >= palabraMasGrandeSize){
+	    		palabraMasGrande = sCadena;
+	    		palabraMasGrandeSize = sCadena.length();
+	    
+	   
+	    	}
+	    	
+	    }
+	    
+	   return palabraMasGrande;
+		
 
 		
-		    
-		}
-
-		public static String masLarga(String cadenaPalabra) {
-			
-			String palabra = cadenaPalabra; 
-			
-			StringTokenizer cadena = new StringTokenizer (palabra);
-			
-			String sCadena;	    
-		    String palabraMasGrande = "";
-		    int palabraMasGrandeSize = 0;
-		    
-		    
-		    int palabras = cadena.countTokens();
-		    
-		    for (int x=0;x<palabras;x++){
-		    	sCadena = cadena.nextToken();
-		    	if (sCadena.length() > palabraMasGrandeSize){
-		    		palabraMasGrande = sCadena;
-		    		palabraMasGrandeSize = sCadena.length();
-		    	}
-		     // 	System.out.println(sCadena);
-		    }
-		    
-		   return ( palabraMasGrande);
-			
-
-			
-		}
+	}
+		
 	
 
 }
--- src/test/cenfotec/kataanalytics/ac/cr/TestAnalytics.java
+++ src/test/cenfotec/kataanalytics/ac/cr/TestAnalytics.java
@@ -3,6 +3,8 @@ package test.cenfotec.kataanalytics.ac.cr;
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.util.LinkedList;
+
 import org.junit.jupiter.api.Test;
 
 import cenfotec.kataanalytics.ac.cr.Analytics;
@@ -13,10 +15,16 @@ public class TestAnalytics {
 	public void testMasLarga() {
 		
 		String palabra = "casa casita casototota";
+		String palabra2 = "luz mueble cartas ";
+		String palabra3 = "ama verde" ;
 		
-		String listaPalabras = Analytics.masLarga(palabra);
+		String p1 = Analytics.masLarga(palabra);
+		String p2 = Analytics.masLarga(palabra2);
+		String p3 = Analytics.masLarga(palabra3);
 				
-		assertEquals("casototota", listaPalabras);
+		assertEquals("casototota", p1);
+		assertEquals("cartas", p2);
+		assertEquals("verde", p3);
 		
 		
 	}
